fd0f02e6d0c47e3122df77e8c2189854f1aa9138
Ketouem <ketouem@gmail.com>
2015-05-15 23:04:27 +0200

    The LightControllerRegistry is now avaible for each request

--- lioght/__init__.py
+++ lioght/__init__.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, g
+
+from light_controller import LightControllerRegistry
 
 
 def create_app():
 
     app = Flask(__name__)
+
+    @app.before_request
+    def before_request():
+        g.registry = LightControllerRegistry
+
     return app
