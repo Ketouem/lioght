1e7cbd056558ef3a1c2c7a0f0e05add2e38df0d4
Ketouem <ketouem@gmail.com>
2015-05-15 21:08:08 +0200

    Provide a shell access to the project via Flask-Script

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask.ext.script import Manager, Shell
+
+from lioght import create_app
+from lioght.light_controller import LightControllerRegistry
+from lioght.light_controller.milight_controller import MiLightController
+
+
+app = create_app()
+manager = Manager(app)
+
+
+def _make_shell_context():
+    return dict(LightControllerRegistry=LightControllerRegistry,
+                MiLightController=MiLightController)
+
+manager.add_command("shell", Shell(make_context=_make_shell_context))
+
+if __name__ == "__main__":
+    manager.run()
